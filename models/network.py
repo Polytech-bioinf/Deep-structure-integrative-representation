5d6a890e0f17b04bb11ffc056afa5b7a319b7e34
yangyan <yang552410127@qq.com>
2022-04-22 11:30:26 +0800

    update

--- models/network.py
+++ models/network.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-import tensorflow as tf
+
 
 class Networks(nn.Module):
     def __init__(self):
--- train_C.py
+++ train_C.py
@@ -1,15 +1,9 @@
 import torch
 from data.dataloader import data_loader_train
 from models.network import Networks
-import models.metrics as metrics
 import numpy as np
-import scipy.io as sio
-from scipy.sparse.linalg import svds
-from sklearn import cluster
-from sklearn.preprocessing import normalize
-import scipy.io
-import os
-import tensorflow as tf
+
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -54,9 +48,6 @@ def load_data(data_name):
     return gene_expression, Img, Label, n_input
 
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
-tf.logging.set_verbosity(tf.logging.ERROR)
-
 data_dir = "./data/BIC/"
 data_name1 = "BREAST_Gene_Expression.txt"
 data_name2 = "BREAST_Methy_Expression.txt"
@@ -74,8 +65,6 @@ s = Bunch()
 s.data = [[Img1], [Img2], [Img3]]
 
 
-regg2 = [1, 5, 10, 20, 50, 100, 150, 200]
-regg3 = [1, 5, 10, 20, 50, 100, 150, 200]
 model = Networks().to(device)
 criterion = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, weight_decay=0.0)
@@ -107,49 +96,43 @@ optimizer2 = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters
 criterion3 = torch.nn.MSELoss(reduction='sum')
 criterion2 = torch.nn.L1Loss(reduction='sum')
 
-y = 0  # 记录reg组合
-for r in range(0, len(regg2)):
-    reg2 = regg2[r]
-    t = 0
-    for h in range(0, len(regg3)):
-        model.load_state_dict(torch.load('./models/BIC.pth', map_location=torch.device('cpu')))
-        optimizer2 = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, weight_decay=0.0)
-        print("step2")
-        print("---------------------------------------")
-        reg3 = regg3[h]
-        l = 0
-        for epoch in range(n_epochs2):
-            for data in data_loader_train:
 
-                train_imga, train_imgb, train_imgc = data
+model.load_state_dict(torch.load('./models/BIC.pth', map_location=torch.device('cpu')))
+optimizer2 = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, weight_decay=0.0)
+print("step2")
+print("---------------------------------------")
+l = 0
+for epoch in range(n_epochs2):
+    for data in data_loader_train:
 
-                input1 = train_imga.view(624, 1, 2000, 1).to(device)
-                input2 = train_imgb.view(624, 1, 2000, 1).to(device)
-                input3 = train_imgc.view(624, 1, 885, 1).to(device)
+        train_imga, train_imgb, train_imgc = data
 
-                z1, z2, z3, output1, output2, output3, zcoef1, zcoef2, zcoef3, coef = model.forward2(input1, input2,
-                                                                                                     input3)
-                loss_re = criterion3(coef, torch.zeros(624, 624, requires_grad=True).to(device))
-                loss_e = criterion3(zcoef2, z2) + criterion3(zcoef1, z1) + criterion3(zcoef3, z3)
-                loss_r = criterion3(output1, input1) + criterion3(output2, input2)+ criterion3(output3, input3)
-                loss_ff = criterion2(coef.mul(torch.tensor(fused_network)), torch.zeros((624, 624)))
-                loss = loss_r + 1 * loss_e + reg2 * loss_re - reg3 * loss_ff
+        input1 = train_imga.view(624, 1, 2000, 1).to(device)
+        input2 = train_imgb.view(624, 1, 2000, 1).to(device)
+        input3 = train_imgc.view(624, 1, 885, 1).to(device)
 
-                optimizer2.zero_grad()
-                loss.backward()
-                optimizer2.step()
-            if epoch % 10 == 0:
+        z1, z2, z3, output1, output2, output3, zcoef1, zcoef2, zcoef3, coef = model.forward2(input1, input2,
+                                                                                             input3)
+        loss_re = criterion3(coef, torch.zeros(624, 624, requires_grad=True).to(device))
+        loss_e = criterion3(zcoef2, z2) + criterion3(zcoef1, z1) + criterion3(zcoef3, z3)
+        loss_r = criterion3(output1, input1) + criterion3(output2, input2)+ criterion3(output3, input3)
+        loss_ff = criterion2(coef.mul(torch.tensor(fused_network)), torch.zeros((624, 624)))
+        loss = loss_r + 1 * loss_e + 10 * loss_re - 20 * loss_ff
 
-                print("Epoch {}/{}".format(epoch, n_epochs2))
-                print("Loss is:{:.4f}".format(loss.item()))
-                commonC = coef.cpu().detach().numpy()
+        optimizer2.zero_grad()
+        loss.backward()
+        optimizer2.step()
+    if epoch % 10 == 0:
+
+        print("Epoch {}/{}".format(epoch, n_epochs2))
+        print("Loss is:{:.4f}".format(loss.item()))
+        commonC = coef.cpu().detach().numpy()
 
 
-                np.savetxt("/Users/yangyan/Desktop/test/BIC/_param_comb/param2_" + str(regg2[y]) + "/param3_" + str(regg3[t]) + "/epoch_" + str(l) + "/matrix" + ".txt", np.c_[commonC],
-                           fmt='%lf', delimiter='\t')
-                torch.save(model.state_dict(), './models/BIC/param2_'+ str(regg2[y]) + '/param3_' + str(regg3[t]) + '/epoch_' + str(l*10) +'.pth')
-                l = l + 1
-        t = t + 1
-    y = y + 1
+        np.savetxt("/Users/yangyan/Desktop/test/BIC/_param_comb/param2_" + str(10) + "/param3_" + str(20) + "/epoch_" + str(l) + "/matrix" + ".txt", np.c_[commonC],
+                   fmt='%lf', delimiter='\t')
+        torch.save(model.state_dict(), './models/BIC/param2_'+ str(10) + '/param3_' + str(20) + '/epoch_' + str(l*10) +'.pth')
+        l = l + 1
+
 
 
